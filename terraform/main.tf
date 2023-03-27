1416d6765258316e627f19951edc244de2c2fbe4
benschwartz9 <schwartzb.2019@gmail.com>
2023-03-26 20:16:14 -0400

    try cloud build

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,10 +9,35 @@ terraform {
 
 provider "google" {
   project = var.project_id
-  region  = "us-east5"
-  zone    = "us-east5-a"
+  region  = "us-east1"
+  zone    = "us-east1-a"
 }
 
-resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
+# resource "google_compute_network" "vpc_network" {
+#   name = "terraform-network"
+# }
+
+resource "google_cloudbuild_trigger" "app-trigger" {
+  location = "us-east1"
+
+  github {
+    owner = "packit461"
+    name = "packit23"
+
+    push {
+      branch = "terraform-container-build"
+    }
+  }
+  included_files = ["test-app/*"] # Only update container if the folder is updated
+
+  build {
+    step {
+      name = "gcr.io/cloud-builders/docker"
+      args = ["build", "-t", "gcr.io/${var.project_id}/quickstart-image:$COMMIT_SHA", "."]
+    }
+    step {
+      name = "gcr.io/cloud-builders/docker"
+      args = ["push", "gcr.io/${var.project_id}/quickstart-image:$COMMIT_SHA"]
+    }    
+  }
 }
