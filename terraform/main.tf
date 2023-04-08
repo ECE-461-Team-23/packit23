94aab22e893790921987b1e1b18ed587d3b3eec7
Emile Baez <ebaezmunne@gmail.com>
2023-04-08 19:12:47 -0400

    something working

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,75 +1,3 @@
-# # Settings
-# locals {
-#   # General
-#   github_branch = "terraform"
-#   artifact_registry_repo_name = "container-repo"
-
-#   # test-app
-#   test_app_cloud_run_name = "test-app"
-#   test_app_image_name = "python-app2"
-
-#   # sql db
-#   google_sql_database_instance = "mysql-instance"
-
-# }
-
-# terraform {
-#   required_providers {
-#     google = {
-#       source  = "hashicorp/google"
-#       version = "4.51.0"
-#     }
-#   }
-# }
-
-# provider "google" {
-#   project = var.project_id
-#   region  = var.region
-#   zone    = "us-central1-a"
-# }
-
-# resource "google_artifact_registry_repository" "container-repo" {
-#   location = var.region
-#   repository_id = local.artifact_registry_repo_name
-#   description   = "Repository to store containers and artifacts"
-#   format        = "DOCKER"
-#   depends_on = [google_project_service.artifact_registry_api]
-# }
-
-# # Automatically build container for test-app
-# resource "google_cloudbuild_trigger" "app-trigger" {
-#   location = var.region
-
-#   github {
-#     owner = "packit461"
-#     name = "packit23"
-
-#     push {
-#       branch = local.github_branch
-#     }
-#   }
-
-#   filename = "test-app/cloudbuild.yaml"
-#   depends_on = [google_project_service.cloud_build_api]
-# }
-
-# ## Enable services ##
-
-# resource "google_project_service" "cloud_run_api" {
-#   service = "run.googleapis.com"
-#   disable_on_destroy = true
-# }
-
-# resource "google_project_service" "cloud_build_api" {
-#   service = "cloudbuild.googleapis.com"
-#   disable_on_destroy = true
-# }
-
-# resource "google_project_service" "artifact_registry_api" {
-#   service = "artifactregistry.googleapis.com"
-#   disable_on_destroy = true
-# }
-
 # # API Gateway
 
 # resource "google_api_gateway_api" "api_gw" {
@@ -103,79 +31,20 @@
 
 # }
 
-# # SQL
-# resource "google_sql_database_instance" "mysql-instance" {
-#   name             = local.google_sql_database_instance
-#   region           = var.region
-#   database_version = "MYSQL_8_0"
-#   settings {
-#     tier = "db-f1-micro"
-#   }
-
-#   deletion_protection  = "false"
-# }
-
-# resource "google_sql_database" "database" {
-#   name = "mysql_db"
-#   instance = local.google_sql_database_instance
-# }
-
-# resource "random_id" "db_name_suffix" {
-#   byte_length = 4
-# }
-
-# resource "google_sql_user" "read-user" {
-#   name     = "read-user"
-#   instance = local.google_sql_database_instance
-#   host     = "%"
-#   password = "strongpassword"
-# }
-
-# Run containers for test-app (container image is overwritten in cloudbuild.yaml)
-# resource "google_cloud_run_service" "run_service" {
-#   name = local.test_app_cloud_run_name
-#   location = var.region
-
-#   template {
-#     spec {
-#       containers {
-#         #image = "us-docker.pkg.dev/cloudrun/container/placeholder:latest" # Placeholder
-#         image = "us-central1-docker.pkg.dev/${var.project_id}/${local.artifact_registry_repo_name}/${local.test_app_image_name}:latest"
-#       }
-#     }
-#   }
-#   # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/cloud_run_service#metadata
-#   # How to connect a container to a SQL database
-
-#   traffic {
-#     percent         = 100
-#     latest_revision = true
-#   }
-
-#   depends_on = [google_project_service.cloud_run_api]  # Waits for the Cloud Run API to be enabled
-# }
-
-# # Allow unauthenticated users to invoke the Cloud Run service
-# resource "google_cloud_run_service_iam_member" "run_all_users" {
-#   service  = google_cloud_run_service.run_service.name
-#   location = google_cloud_run_service.run_service.location
-#   role     = "roles/run.invoker"
-#   member   = "allUsers"
-# }
-
-# output "service_url" {
-#   value = google_cloud_run_service.run_service.status[0].url
-# }
 
 # Settings
 locals {
   # General
   github_branch = "terraform"
   artifact_registry_repo_name = "container-repo"
+  region = "us-central1"
 
   # package-rater-app
   package_rater_app_cloud_run_name = "package-rater-app"
   package_rater_app_image_name = "package-rater-image"
+
+  # SQL
+  google_sql_database_instance = "mysql-instance"
 }
 
 terraform {
@@ -189,12 +58,12 @@ terraform {
 
 provider "google" {
   project = var.project_id
-  region  = "us-central1"
+  region  = local.region
   zone    = "us-central1-a"
 }
 
 resource "google_artifact_registry_repository" "container_repo" {
-  location = "us-central1"
+  location = local.region
   repository_id = local.artifact_registry_repo_name
   description   = "Repository to store containers and artifacts"
   format        = "DOCKER"
@@ -303,9 +172,37 @@ resource "google_secret_manager_secret_iam_member" "package_rater_access" {
   member = "serviceAccount:${google_service_account.package_rater_service_account.email}"
 }
 
-
 output "service_url" {
   value = google_cloud_run_service.run_service.status[0].url
+}
+
+# SQL Database
+resource "google_sql_database_instance" "mysql-instance" {
+  name             = local.google_sql_database_instance
+  region           = local.region
+  database_version = "MYSQL_8_0"
+  settings {
+    tier = "db-f1-micro"
+  }
+
+  deletion_protection  = "true"
+}
+
+resource "google_sql_database" "database" {
+  name = "mysql_db"
+  instance = local.google_sql_database_instance
+}
+
+resource "random_id" "db_name_suffix" {
+  byte_length = 4
+}
+
+# SQL users
+resource "google_sql_user" "read-user" {
+  name     = "read-user"
+  instance = local.google_sql_database_instance
+  host     = "%"
+  password = "strongpassword"
 }
 
 ## Service Accounts ##
