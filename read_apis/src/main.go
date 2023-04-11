8c7796f021445ae21305e1c5379c12d589ab4f5b
Ashassins <ashenassassinmc@gmail.com>
2023-04-10 20:50:10 -0400

    ADD: handle_packages

--- read_apis/src/main.go
+++ read_apis/src/main.go
@@ -16,12 +16,25 @@ import (
 
 	"cloud.google.com/go/cloudsqlconn"
 	"cloud.google.com/go/cloudsqlconn/mysql/mysql"
+	_ "github.com/go-sql-driver/mysql"
 )
+
+type RegistryPackage struct {
+	ID          int
+	NAME        string
+	RATING_PK   int
+	AUTHOR_PK   int
+	URL         string
+	BINARY_PK   int
+	VERSION     string
+	UPLOADED    int // datetime?
+	IS_EXTERNAL bool
+}
 
 func connect() {
 	cleanup, err := mysql.RegisterDriver("cloudsql-mysql", cloudsqlconn.WithCredentialsFile("key.json"))
 	if err != nil {
-		// ... handle error
+		log.Fatal(err)
 	}
 	// call cleanup when you're done with the database connection
 	defer cleanup()
@@ -30,12 +43,35 @@ func connect() {
 		"cloudsql-mysql",
 		"myuser:mypass@cloudsql-mysql(project:region:instance)/mydb",
 	)
-	// ... etc
+
+	if db != nil {
+		fmt.Print("Db not nil!")
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 // Get the packages from the registry
 func handle_packages(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	db, err := sql.Open("mysql", "user7:s$cret@tcp(127.0.0.1:3306)/testdb") // EMILE FIX PLS
+	defer db.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	res, err := db.Query(`BEGIN
+		SELECT * FROM Registry AS A 
+		INNER JOIN Binaries AS B 
+			ON A.BINARY_PK = B.ID 
+		INNER JOIN Users AS C 
+			ON A.USER_PK = C.ID 
+		INNER JOIN Ratings AS D 
+			ON A.RATING_PK = D.ID 
+		END;`)
+	defer res.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 // Return this package (ID)
