b0cd76e673aa751e862dfffb4c6cdcdb9814fa6e
benschwartz9 <schwartzb.2019@gmail.com>
2023-04-12 13:25:32 -0400

    Swap Flask to FastAPI, added Auth endpoint, boilerplate code for upload package

--- delete_write_apis/src/delete.py
+++ delete_write_apis/src/delete.py
@@ -4,25 +4,42 @@ APIs for DELETE methods:
 /package/(id)
 /package/byName/(name)
 """
-from src import app
 
-# FOR TESTING PURPOSES
-@app.route("/", methods=["GET"])
-def test():
-    """Func description"""
-    return "Hello"
+from fastapi import APIRouter
 
-@app.route("/reset", methods=["DELETE"])
-def reset():
-    """Func description"""
-    return "Reset"
+router = APIRouter()
 
-@app.route("/package/<string:id>", methods=["DELETE"])
-def package_id_delete(id):
-    """Func description"""
-    return "Package by ID"
+@router.get("/delete")
+def delete_root():
+    return {"Hello": "Delete"}
 
-@app.route("/package/byName/<string:id>", methods=["DELETE"])
-def package_byname_delete(id):
-    """Func description"""
-    return "Package by name"
+# @router.delete('/reset', response_model=None)
+# def registry_reset(
+#     x__authorization: AuthenticationToken = Header(..., alias='X-Authorization')
+# ) -> None:
+#     """
+#     Reset the registry
+#     """
+#     pass
+
+# @router.delete('/package/byName/{name}', response_model=None)
+# def package_by_name_delete(
+#     name: PackageName,
+#     name: PackageName = ...,
+#     x__authorization: AuthenticationToken = Header(..., alias='X-Authorization'),
+# ) -> None:
+#     """
+#     Delete all versions of this package.
+#     """
+#     pass
+
+# @router.delete('/package/{id}', response_model=None)
+# def package_delete(
+#     id: PackageID,
+#     id: PackageID = ...,
+#     x__authorization: AuthenticationToken = Header(..., alias='X-Authorization'),
+# ) -> None:
+#     """
+#     Delete this version of the package.
+#     """
+#     pass
--- delete_write_apis/src/write.py
+++ delete_write_apis/src/write.py
@@ -7,25 +7,134 @@ APIs for POST methods:
 function names will have method specified if already
 in use (other apis) 
 """
-from src import app
 
-# FOR TESTING PURPOSES
-@app.route("/index", methods=["GET"])
-def index():
-    """Func description"""
-    return "Hello again"
+import json
+import traceback
 
-@app.route("/package", methods=["POST"])
-def package():
-    """Func description"""
-    return "Package"
 
-@app.route("/package/<string:id>", methods=["POST"])
-def package_id_post(id):
-    """Func description"""
-    return "Package by ID"
+from typing import List, Optional, Union
+from fastapi import APIRouter, Request, HTTPException, Header, Body
 
-@app.route("/authenticate", methods=["POST"])
-def authenticate():
-    """Func description"""
-    return "Authenticate"
+from . import authentication
+
+from .models import (
+    AuthenticationRequest,
+    AuthenticationToken,
+    EnumerateOffset,
+    Error,
+    Package,
+    PackageData,
+    PackageHistoryEntry,
+    PackageID,
+    PackageMetadata,
+    PackageName,
+    PackageQuery,
+    PackageRating,
+)
+
+router = APIRouter()
+
+@router.get("/write")
+def write_root():
+    return {"Hello": "Write"}
+
+test_db = {
+    "sampleuser": b'$2b$12$iUwbqGXqpky94Xul/l0RMe.nIe0HGFha9eQ0M9.82MIeSNvxoXNje'
+}
+
+@router.put('/authenticate', response_model=AuthenticationToken)
+async def create_auth_token(request: Request) -> AuthenticationToken:
+    # Parsing to make sure valid request (Need to manually decode request to allow unicode characters)
+    try:
+        payload = await request.body()
+        payloadDecoded = payload.decode("UTF-8")
+        parsed_body = json.loads(payloadDecoded, strict=False)
+
+        username = parsed_body["User"]["name"]
+        password = parsed_body["Secret"]["password"]
+
+        # get_hashed_password is only ran by an administrator to add users by hand
+        # print("New password:")
+        # print(authentication.get_hashed_password(password)) 
+    except Exception:
+        print(f"Unable to get/parse request body: {traceback.print_exc()}")
+        raise HTTPException(status_code=400, detail="Unable to get/parse request body")
+
+    # See if username and password are valid
+    try:
+        stored_password = test_db[username]
+        assert stored_password != None
+        assert authentication.check_password(password, stored_password)
+    except Exception:
+        print(f"The user or password is invalid: {traceback.print_exc()}")
+        raise HTTPException(status_code=401, detail="The user or password is invalid")
+
+    # Generate and return token
+    try:
+        token = authentication.generate_jwt(username)
+        assert token != None
+        return AuthenticationToken(__root__=token)
+    except Exception:
+        print(f"Error when trying to generate token: {traceback.print_exc()}")
+        raise HTTPException(status_code=501, detail="Internal server error")
+
+
+@router.post('/package', response_model=None, status_code=201)
+async def package_create(request: Request) -> Union[None, Package]:
+    # Parse request
+    try:
+        token = request.headers["X-Authorization"]
+        userid = authentication.validate_jwt(token)
+        assert userid != None
+
+        payload = await request.body()
+        payloadDecoded = payload.decode("UTF-8")
+        parsed_body = json.loads(payloadDecoded, strict=False)
+
+        # On package upload, either Content or URL should be set.
+        assert ("Content" in parsed_body) or ("URL" in parsed_body) # At least one should be set
+        assert not ( ("Content" in parsed_body) and ("URL" in parsed_body) ) # Both shouldn't be set
+
+        # TODO: Is this required?
+        # assert "JSProgram" in parsed_body
+    except Exception:
+        print(f"There is missing field(s) in the PackageData/AuthenticationToken or it is formed improperly, or the AuthenticationToken is invalid: {traceback.print_exc()}")
+        raise HTTPException(status_code=400, detail="There is missing field(s) in the PackageData/AuthenticationToken or it is formed improperly, or the AuthenticationToken is invalid.")
+
+
+    # Handle Request
+    print(parsed_body)
+    if "Content" in parsed_body:
+        # Upload package
+        pass
+    elif "URL" in parsed_body:
+        # Ingest package from public URL
+        pass
+
+
+    # Package results
+    packageMetadata = PackageMetadata(
+        Name=PackageName(__root__="ExampleName"),
+        Version="ExampleVersion",
+        ID=PackageID(__root__="ExampleName"),
+    )
+    packageData = PackageData(Content="Example Content")
+    # packageData = PackageData(URL="Example URL") # TODO: is this an option?
+
+    return Package(metadata=packageMetadata, data=packageData)
+
+
+
+# @router.put('/package/{id}', response_model=None)
+# def package_update(
+#     id: PackageID,
+#     id: PackageID = ...,
+#     x__authorization: AuthenticationToken = Header(..., alias='X-Authorization'),
+#     body: Package = ...,
+# ) -> None:
+#     """
+#     Update this content of the package.
+#     """
+#     pass
+
+
