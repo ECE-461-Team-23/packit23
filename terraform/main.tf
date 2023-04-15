f2e0d1615910edbca1287efa4de430c35daf8e2b
benschwartz9 <schwartzb.2019@gmail.com>
2023-04-14 23:36:14 -0400

    add back in database

--- terraform/main.tf
+++ terraform/main.tf
@@ -432,26 +432,26 @@ output "write_apis_service_url" {
 }
 
 # SQL Database
-# resource "google_sql_database_instance" "mysql-instance" {
-#   name             = local.mysql_db_instance_name
-#   region           = local.region
-#   database_version = "MYSQL_8_0"
-#   settings {
-#     tier = "db-f1-micro"
-#     disk_size = 10 # GB
-#   }
+resource "google_sql_database_instance" "mysql-instance" {
+  name             = local.mysql_db_instance_name
+  region           = local.region
+  database_version = "MYSQL_8_0"
+  settings {
+    tier = "db-f1-micro"
+    disk_size = 10 # GB
+  }
 
-#   deletion_protection  = "true"
-# }
+  deletion_protection  = "true"
+}
 
-# resource "google_sql_database" "database" {
-#   name = local.mysql_db_name
-#   instance = local.mysql_db_instance_name
-# }
+resource "google_sql_database" "database" {
+  name = local.mysql_db_name
+  instance = local.mysql_db_instance_name
+}
 
-# resource "random_id" "db_name_suffix" {
-#   byte_length = 4
-# }
+resource "random_id" "db_name_suffix" {
+  byte_length = 4
+}
 
 # SQL users
 resource "google_sql_user" "read-user" {
@@ -486,9 +486,7 @@ resource "google_api_gateway_api_config" "api_cfg" {
   openapi_documents {
     document {
       path = "api_spec.yaml"
-      #contents = base64encode(templatefile("${path.module}/api_spec.tpl", { read_url=google_cloud_run_service.read_apis_run_service.status[0].url, write_url=google_cloud_run_service.write_apis_run_service.status[0].url }))
       contents = base64encode(templatefile("${path.module}/api_spec.yaml", { read_url=google_cloud_run_service.write_apis_run_service.status[0].url, write_url=google_cloud_run_service.write_apis_run_service.status[0].url }))
-    
     }
   }
   lifecycle {
