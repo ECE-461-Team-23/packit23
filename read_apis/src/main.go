686d75e1ec7bc9c69ccf7592b6dbcc1858d2957b
benschwartz9 <schwartzb.2019@gmail.com>
2023-04-15 00:10:36 -0400

    clean up unrelated code

--- read_apis/src/main.go
+++ read_apis/src/main.go
@@ -1,4 +1,4 @@
-package src
+package main
 
 /*
 Provide GET functionality for the following endpoints:
@@ -9,176 +9,19 @@ Provide GET functionality for the following endpoints:
 */
 
 import (
-	"context"
-	"database/sql"
-	"encoding/json"
 	"fmt"
-	"io"
 	"log"
-	"net"
 	"net/http"
-	"os"
-
-	"github.com/packit461/packit23/package_rater/internal/logger"
-
-	"cloud.google.com/go/cloudsqlconn"
-	"cloud.google.com/go/cloudsqlconn/mysql/mysql"
-	_ "github.com/go-sql-driver/mysql"
 )
-
-func connect() (*sql.DB, error) {
-	mustGetenv := func(k string) string {
-		v := os.Getenv(k)
-		if v == "" {
-			log.Fatalf("Fatal Error in connect_connector.go: %s environment variable not set.", k)
-		}
-		return v
-	}
-
-	var (
-		dbUser   = mustGetenv("DB_USER")
-		dbPwd    = mustGetenv("DB_PASSWORD")
-		dbName   = mustGetenv("DB_NAME")
-		project  = mustGetenv("PROJECT_ID")
-		region   = mustGetenv("REGION")
-		instance = mustGetenv("INSTANCE_NAME")
-
-		usePrivate = os.Getenv("PRIVATE_IP")
-	)
-
-	var instanceConnectionName = project + region + instance
-
-	d, err := cloudsqlconn.NewDialer(context.Background())
-	if err != nil {
-		return nil, fmt.Errorf("cloudsqlconn.NewDialer: %v", err)
-	}
-	var opts []cloudsqlconn.DialOption
-	if usePrivate != "" {
-		opts = append(opts, cloudsqlconn.WithPrivateIP())
-	}
-	mysql.RegisterDialContext("cloudsqlconn",
-		func(ctx context.Context, addr string) (net.Conn, error) {
-			return d.Dial(ctx, instanceConnectionName, opts...)
-		})
-
-	dbURI := fmt.Sprintf("%s:%s@cloudsqlconn(localhost:3306)/%s?parseTime=true",
-		dbUser, dbPwd, dbName)
-
-	dbPool, err := sql.Open("mysql", dbURI)
-	if err != nil {
-		return nil, fmt.Errorf("sql.Open: %v", err)
-	}
-	return dbPool, nil
-}
-
-func return_error_packet(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusInternalServerError)
-	w.Write([]byte("500 - Internal error"))
-}
 
 // Get the packages from the registry
 func handle_packages(w http.ResponseWriter, r *http.Request) {
-	db, err := sql.Open("mysql", "user7:s$cret@tcp(127.0.0.1:3306)/testdb") // EMILE FIX PLS
-	defer db.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	logger.Info(fmt.Sprintf("Received %s request", r.Method))
-	headers := "Headers:\n"
-	for key, value := range r.Header {
-		headers += fmt.Sprintf("%s=%s\n", key, value)
-	}
-	logger.Info(headers)
-
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		logger.Info("\nError reading body of request\n")
-		return_error_packet(w, r)
-		return
-	}
-	logger.Info(fmt.Sprintf("Body:\n%s\n", body))
-	// TODO: ADD PAGINATION STUFF
-	res, err := db.Query(`SELECT ID, NAME, VERSION FROM Registry;`)
-	defer res.Close()
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// --------- DEBUGGING/EXPERIMENTAL CODE TO VIEW RETURN ---------
-	for res.Next() {
-		var pack PackageMetadata // I have no idea what to put here
-		err := res.Scan(&pack.ID, &pack.VERSION, &pack.NAME)
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Printf("%v\n", pack)
-	}
-	// --------------------------------------------------------------
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
-
-// `BEGIN
-//	SELECT * FROM Registry AS A
-//	INNER JOIN Binaries AS B
-//		ON A.BINARY_PK = B.ID
-//	INNER JOIN Users AS C
-//		ON A.USER_PK = C.ID
-//	INNER JOIN Ratings AS D
-//		ON A.RATING_PK = D.ID
-//	END;`
 
 // Return this package (ID)
 func handle_packages_id(w http.ResponseWriter, r *http.Request) {
-	db, err := sql.Open("mysql", "user7:s$cret@tcp(127.0.0.1:3306)/testdb") // EMILE FIX PLS
-	defer db.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	logger.Info(fmt.Sprintf("Received %s request", r.Method))
-	headers := "Headers:\n"
-	for key, value := range r.Header {
-		headers += fmt.Sprintf("%s=%s\n", key, value)
-	}
-	logger.Info(headers)
-
-	id := r.Header.Get("Id")
-	if id == "" {
-		logger.Info("\nNo Matching Value to key Id\n")
-		return_error_packet(w, r)
-		return
-	}
-
-	res, err := db.Query(`SELECT ID, NAME, VERSION FROM Registry;`)
-	defer res.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
-	var meta PackageMetadata
-	err = res.Scan(&meta.ID, &meta.Name, &meta.Version)
-	res, err = db.Query(`BEGIN SELECT 
-							B.BINARY_FILE, 
-							A.URL 
-							B.JS_PROGRAM
-							FROM Registry AS A
-							WHERE A.ID == id
-							INNER JOIN Binaries AS B
-								ON A.BINARY_PIK == B.ID
-							END;`)
-	defer res.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
-	var packData PackageData
-	// Need to append NULL for JSProgram
-	err = res.Scan(&packData.Content, &packData.URL, &packData.JSProgram)
-	totalPack := PackageModel{Metadata: &meta, Data: &packData}
-	packJson, err := json.Marshal(totalPack)
-	w.Write(packJson)
-	w.WriteHeader(200)
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
 // Return the rating. Only use this if each metric was computed successfully.
