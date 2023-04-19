da6d262de11018759ce39654d466b0e60cdc5735
Ashassins <ashenassassinmc@gmail.com>
2023-04-19 16:50:27 -0400

    ADD: JS Structure

---
+++ docs/js/registry.js
@@ -0,0 +1,48 @@
+// Populate RegistryTable
+// This should use /packages endpoint
+function renderTable() {
+    $.ajax({
+        'url': "/packages",
+        'method': "POST",
+        'contentType': 'application/json'
+    }).done( function(data) {
+        $('#registryTable').dataTable( {
+            "aaData": data,
+            "columns": [
+                { "data": "ID"},
+                { 
+                    "data": "Name", 
+                    "render": function(data, type, row, meta) {
+                        return '<a href="#" onclick="openEntry(' + data + ')">' + data + '</a>';
+                    } 
+                },
+                { "data": "Version" },
+            ]
+        })
+    })
+        
+    // "<i href=openEntry(" Data.ID") class='bi bi-folder2-open'> </i>"
+    
+    // button onClick=""
+    // onRowClick("my-table-id", function (row){
+    //     var value = row.getElementsByTagName("td")[0].innerHTML;
+    //     document.getElementById('click-response').innerHTML = value + " clicked!";
+    //     console.log("value>>", value);
+    // });
+}
+
+function deleteEntry() {
+
+}
+
+function editEntry() {
+
+}
+
+function openEntry(id) {
+    // get by package by ID and open 
+}
+
+function searchRegistry() {
+
+}
